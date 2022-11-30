afc4258af3f888d8f48fa9fb546054a9d786cb12
kulotsystems <kulotsystems@gmail.com>
2022-11-30 12:06:20 +0800

    Lesson 7: Adding Tailwind CSS.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <nuxt-link to="/">Nuxt Dojo</nuxt-link>
-                <ul>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <nuxt-link to="/" class="font-bold">Nuxt Dojo</nuxt-link>
+                <ul class="flex gap-4">
                     <li><nuxt-link to="/">Home</nuxt-link></li>
                     <li><nuxt-link to="/about">About</nuxt-link></li>
-                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                    <li><nuxt-link to="/products" class="btn">Products</nuxt-link></li>
                 </ul>
             </nav>
         </header>
 
         <!-- Page Content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot/>
         </div>
     </div>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <nuxt-link to="/products">Nuxt Dojo Merch</nuxt-link>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <nuxt-link to="/products" class="font-bold">Nuxt Dojo Merch</nuxt-link>
             </nav>
         </header>
 
         <!-- Page Content -->
-        <slot/>
+        <div class="container mx-auto p-4">
+            <slot/>
+        </div>
 
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li><nuxt-link to="/">Home</nuxt-link></li>
                 <li><nuxt-link to="/about">About</nuxt-link></li>
                 <li><nuxt-link to="/products">Products</nuxt-link></li>
