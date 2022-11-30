5f772da277571caab702e2453285605b738342d2
kulotsystems <kulotsystems@gmail.com>
2022-11-30 11:19:53 +0800

    Lesson 5: NuxtLink.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Nuxt Dojo</nuxt-link>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                </ul>
+            </nav>
+        </header>
+
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius eum facere ipsa iste iusto laborum veniam vero voluptates voluptatibus?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi cumque debitis earum, et minima natus neque perferendis repellendus voluptas.</p>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Nuxt Dojo</nuxt-link>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                </ul>
+            </nav>
+        </header>
+
         <h2>Products</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cupiditate doloremque earum fuga in nobis perferendis ratione ullam velit voluptates!</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorum impedit nostrum voluptates. Amet corporis repellat reprehenderit totam vero voluptatem.</p>
