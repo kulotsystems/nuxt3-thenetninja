a7c471f7bdd5944129fa5e7e71ef7b590bd102d5
kulotsystems <kulotsystems@gmail.com>
2022-11-30 10:46:12 +0800

    Lesson 4: Route Parameters.

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt dolorum eius facere facilis, inventore nesciunt obcaecati rem repellat saepe.</p>
+    </div>
+</template>
+
+
+<script setup>
+    const { id } = useRoute().params;
+</script>
+
+
+<style scoped>
+
+</style>
